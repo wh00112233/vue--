8a8197746b29bcd271436543b32c934aac4d1b49
wh00112233 <68369672+wh00112233@users.noreply.github.com>
2020-08-19 10:30:22 +0800

    element-ui

--- src/main.js
+++ src/main.js
@@ -5,9 +5,14 @@ import App from './App'
 import router from './router'
 import Axios from 'axios'
 import vueAxios from 'vue-axios'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import store from './store'
 
 Vue.prototype.$http=Axios
 
+Vue.use(ElementUI);
 Vue.use(vueAxios,Axios)
 
 Vue.config.productionTip = false
@@ -16,6 +21,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
